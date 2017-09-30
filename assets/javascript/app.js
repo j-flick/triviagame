17ba820589fde77494467410be64531b24de617d
Justin Flick <jflick84@gmail.com>
2017-09-30 00:48:31 -0400

    working loop, but timer doubles on reset and correct/wrong answers double on second question.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,8 @@
-// Pseudo-code TO DO:
-// When answer is clicked, move to next question.
-// If no answer is selected, add one to unanswered and move to next question.
-// Reset timer, question, and choices.
-// When there are no more questions only show results.
-// Ask to play again, and reset game.
-
 // Functions to run when the page loads.
 // ====================================================================
 window.onload = function () {
-	$("#startButton").click(playGame);
-	$("#startButton").click(startTimer);
+	$("#startButton").click(startGame);
+	// $("#startButton").click(getQuestion);
 };
 
 // Variables
@@ -17,90 +10,185 @@ window.onload = function () {
 
 // Start time of 30 seconds for each question.
 var time = 30;
-// Holds interval ID when startTimer function is executed.
-var intervalId;
+// Holds a variable for the time interval between questions.
+var timer;
+// Keep track of the index of the current question displayed.
+var currentQuestion = 0;
 
-// Create an object to hold questions for the game.
-var questions = {
-	q1: ["How many new computer viruses appear each month?", "5,000", "100,000", "1,000", "2,500", "5,000"],
-	q2: ["What is the longest word you can type using only one row of letters on the keyboard?", "Typewriter", "Qwerty", "Cascading", "Javascript", "Typewriter"]
-};
+// Create an object to hold questions, choices, and answers for the game.
+var questions = [{
+	question: "How many new computer viruses appear each month?",
+	choices: ["5,000", "100,000", "1,000", "2,500"],
+	answer: "5,000"
+}, {
+	question: "What is the longest word you can type using only one row of letters on the keyboard?",
+	choices: ["Qwerty", "Typewriter", "Computer", "Javascript"],
+	answer: "Typewriter"
+}];
 
-// Variables to hold number of answers right and wrong.
+// Variables to hold number of answers right, wrong, and unanswered.
 var correctAnswers = 0;
 var wrongAnswers = 0;
+var unanswered = 0;
 var answer;
 
 // Functions
 // ====================================================================
 
-function playGame() {
-	// Remove the div with the start button.
+function startGame() {
+	// Remove the div with the start button and get the first question.
 	$("#startDiv").remove();
+	getQuestion(currentQuestion);
+}
 
-	// Show the time remaining.
-	$("#timeDiv").show();
+function displayChoices() {
+	// Hide the display message showing whether answer is right or wrong.
+	// This will be empty on the first go around, but needs to be hidden on subsequent intervals.
+	$("#message").hide();
 
-	// Display question and answer choices.
-	$("#question").html(questions.q1[0]);
+	// Display question.
+	$("#question").css("display", "block");
+	$("#question").html(questions[currentQuestion].question);
 
+	// Display answer choices.
 	$("#choices").css("display", "block");
-
 	// Loop thru to display choices.
 	var choiceLetters = ["A", "B", "C", "D"];
-
-	for (i = 0; i <= 4; i++){
-		$(".choice" + choiceLetters[i]).html(questions.q1[i + 1]);
+	for (i = 0; i < 4; i++){
+		$(".choice" + choiceLetters[i]).html(questions[currentQuestion].choices[i]);
 	}
+}
+
+// Function to show message displaying whether the user got the question right or wrong.
+// Hides other divs so just the message is displayed.
+function showMessage() {
+	$("#message").show();
+	$("#question").hide();
+	$("#choices").hide();
+	$("#timeDiv").hide();
+}
+
+function nextQuestion() {
+	// Move to next question.
+	currentQuestion++;
+	// Stop the timer.
+	clearInterval(timer);
+	// Reset the time to 30 seconds.
+	time = 30;
+	// Get the next question after 4 seconds.
+	setTimeout(getQuestion, 4000);
+}
+
+function getQuestion() {
+	// Run the decrement function every 1 second for a running clock.
+	timer = setInterval(decrement, 1000);
+
+	// Display the div holding the time.
+	$("#timeDiv").css("display", "block");
+
+	// Function displaying the time in HTML.
+	setTime();
+
+	// Function to display question and answer choices.
+	displayChoices();
 
 	// Get answer on click.
 	$(".choice").on("click", function() {
 		answer = $(this).text();
-		console.log(answer);
 
 		// Compare the selected answer to the correct answer.
-		// If they match, add one to correct answers total.
-		if (answer === questions.q1[5]) {
-			console.log("Correct");
+		// If they match, add one to correct answers total and show a message letting the user know they were right.
+		if (answer === questions[currentQuestion].answer) {
 			correctAnswers++;
-			$("#correctAnswers").text(correctAnswers);
-			$("#message").html("Correct!");
+			// Function shows message while hiding the other divs.
+			showMessage();
+			$("#message").html("Correct!<br>Get ready for the next question!");
 		}
-		// If they don't match, add one to the wrong answers total.
+		// If they don't match, add one to the wrong answers total and show a message letting the user know they were wrong.
 		else {
-			console.log("Wrong");
 			wrongAnswers++;
-			$("#wrongAnswers").text(wrongAnswers);
-			$("#message").html("Wrong. The correct answer was " + questions.q1[5] + ".");
+			showMessage();
+			$("#message").html("Wrong. The correct answer was " + questions[currentQuestion].answer + ".<br>Get ready for the next question!");
 		}
 
-		displayResults();
+		// If there are more questions, display the next question.
+		if (currentQuestion < questions.length - 1) {
+			nextQuestion();
+		}
+		// If no questions remain, show the results.
+		else if (currentQuestion === questions.length - 1) {
+			displayResults();
+		}
 
 	});
-
 }
 
 function displayResults() {
+	// Stop the timer.
+	clearInterval(timer);
+	// Show results div with updated user stats.
 	$("#results").show();
+	$("#message").html("Thanks for playing, here are your results!");
+	$("#correctAnswers").text(correctAnswers);
+	$("#wrongAnswers").text(wrongAnswers);
+	$("#unanswered").text(unanswered);
+	// Click listener to see if the user wants to play again.
+	$("#playAgain").on("click", function() {
+		resetGame();
+	});
+}
+
+// Function resets all values to their original settings and starts the game over when the user clicks to play again.
+function resetGame() {
+	// Hide the previous game's results.
+	$("#results").hide();
+	// Reset values.
+	time = 30;
+	correctAnswers = 0;
+	wrongAnswers = 0;
+	unanswered = 0;	
+	currentQuestion = 0;
+	// Get first question again.
+	getQuestion(currentQuestion);
 }
 
 
 // Timer
 // ====================================================================
+// HTML to display the clock.
+function setTime() {
+	$("#timeDiv").html(`<p>Time left: <span id="timeDisplay">${time}</span> seconds...</p>`);
+}
+
 function startTimer() {
-	intervalId = setInterval(decrement, 1000);
+	timer = setInterval(decrement, 1000);
+}
+
+function timesUp() {
+	// Stop the timer.
+	clearInterval(timer);
+	// Add one to the unanswered score since the user did not answer in time.
+	unanswered++;
+	// Show a message displaying the correct answer.
+	showMessage();
+	$("#message").html("Times Up!<br>The correct answer was " + questions[currentQuestion].answer + ".");
+	// If there are no more questions, show the results after 4 seconds.
+	if(currentQuestion == questions.length - 1) {
+		setTimeout(displayResults, 4000);
+	}
+	// If there are more questions, go to the next question after 4 seconds.
+	else {
+		setTimeout(nextQuestion, 4000);
+	}
 }
 
 function decrement() {
 	// Decrease time by 1.
 	time--;
-
 	// Update the HTML with the current time.
-	$("#timeDisplay").html(time);
-
-	// If the time runs out, stop the timer and display an alert.
-	if(time === 0) {
-		alert("Times Up!");
-		clearInterval(intervalId);
+	setTime();
+	// If time runs out, run the function to see if there are more questions left.
+	if (time <= 0) {
+		timesUp();
 	}
 }
