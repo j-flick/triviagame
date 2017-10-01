528d0bfd26999643e2982dd171aa4727fbb8ccc4
Justin Flick <jflick84@gmail.com>
2017-10-01 18:16:08 -0400

    fixed correct/wrong answers so they no longer double on the second question

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,7 +15,7 @@ var timer;
 // Keep track of the index of the current question displayed.
 var currentQuestion = 0;
 
-// Create an object to hold questions, choices, and answers for the game.
+// Create an array of objects to hold questions, choices, and answers for the game.
 var questions = [{
 	question: "How many new computer viruses appear each month?",
 	choices: ["5,000", "100,000", "1,000", "2,500"],
@@ -38,7 +38,7 @@ var answer;
 function startGame() {
 	// Remove the div with the start button and get the first question.
 	$("#startDiv").remove();
-	getQuestion(currentQuestion);
+	getQuestion();
 }
 
 function displayChoices() {
@@ -69,10 +69,10 @@ function showMessage() {
 }
 
 function nextQuestion() {
-	// Move to next question.
-	currentQuestion++;
-	// Stop the timer.
-	clearInterval(timer);
+	// // Move to next question.
+	// currentQuestion++;
+	// // Stop the timer.
+	// clearInterval(timer);
 	// Reset the time to 30 seconds.
 	time = 30;
 	// Get the next question after 4 seconds.
@@ -80,14 +80,15 @@ function nextQuestion() {
 }
 
 function getQuestion() {
+	console.log("Current Question begin getQ:" + currentQuestion);
+	// Function displaying the time in HTML.
+	setTime();
 	// Run the decrement function every 1 second for a running clock.
-	timer = setInterval(decrement, 1000);
+	startTimer();
+	// timer = setInterval(decrement, 1000);
 
 	// Display the div holding the time.
 	$("#timeDiv").css("display", "block");
-
-	// Function displaying the time in HTML.
-	setTime();
 
 	// Function to display question and answer choices.
 	displayChoices();
@@ -99,31 +100,49 @@ function getQuestion() {
 		// Compare the selected answer to the correct answer.
 		// If they match, add one to correct answers total and show a message letting the user know they were right.
 		if (answer === questions[currentQuestion].answer) {
+			// Stop the timer.
+			clearInterval(timer);
 			correctAnswers++;
 			// Function shows message while hiding the other divs.
 			showMessage();
 			$("#message").html("Correct!<br>Get ready for the next question!");
+			// Move to next question.
+			currentQuestion++;
+			console.log("Current Question after correct answer:" + currentQuestion);
 		}
 		// If they don't match, add one to the wrong answers total and show a message letting the user know they were wrong.
 		else {
+			// Stop the timer.
+			clearInterval(timer);
 			wrongAnswers++;
 			showMessage();
 			$("#message").html("Wrong. The correct answer was " + questions[currentQuestion].answer + ".<br>Get ready for the next question!");
+			// Move to next question.
+			currentQuestion++;
+			console.log("Current Question after wrong answer:" + currentQuestion);
+		}
+
+		if (currentQuestion === questions.length) {
+			displayResults();
+		}
+		else {
+			nextQuestion();
 		}
 
 		// If there are more questions, display the next question.
-		if (currentQuestion < questions.length - 1) {
-			nextQuestion();
-		}
-		// If no questions remain, show the results.
-		else if (currentQuestion === questions.length - 1) {
-			displayResults();
-		}
+		// if (currentQuestion < questions.length - 1) {
+		// 	nextQuestion();
+		// }
+		// // If no questions remain, show the results.
+		// else if (currentQuestion === questions.length - 1) {
+		// 	displayResults();
+		// }
 
 	});
 }
 
 function displayResults() {
+	console.log("Current Question at display results:" + currentQuestion);
 	// Stop the timer.
 	clearInterval(timer);
 	// Show results div with updated user stats.
@@ -133,9 +152,10 @@ function displayResults() {
 	$("#wrongAnswers").text(wrongAnswers);
 	$("#unanswered").text(unanswered);
 	// Click listener to see if the user wants to play again.
-	$("#playAgain").on("click", function() {
-		resetGame();
-	});
+	// $("#playAgain").on("click", function() {
+	// 	resetGame();
+	// });
+	$("#playAgain").click(resetGame);
 }
 
 // Function resets all values to their original settings and starts the game over when the user clicks to play again.
@@ -147,9 +167,11 @@ function resetGame() {
 	correctAnswers = 0;
 	wrongAnswers = 0;
 	unanswered = 0;	
+	console.log("Current Question before reset:" + currentQuestion);
 	currentQuestion = 0;
+	console.log("Current Question after reset:" + currentQuestion);
 	// Get first question again.
-	getQuestion(currentQuestion);
+	getQuestion();
 }
 
 
@@ -173,7 +195,7 @@ function timesUp() {
 	showMessage();
 	$("#message").html("Times Up!<br>The correct answer was " + questions[currentQuestion].answer + ".");
 	// If there are no more questions, show the results after 4 seconds.
-	if(currentQuestion == questions.length - 1) {
+	if(currentQuestion === questions.length - 1) {
 		setTimeout(displayResults, 4000);
 	}
 	// If there are more questions, go to the next question after 4 seconds.
